36440866dc7be30956372fac395ecdf048ea04c0
Mohamed Ashif <mohamed.ashif@impacteers.com>
2025-03-13 15:12:52 +0530

    frontend check secret

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { createHmac } from "crypto";
 
 export function useAuth() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -10,10 +11,29 @@ export function useAuth() {
     const token = localStorage.getItem("token");
 
     if (!token) {
-      //   router.push("/login");
       router.push("/");
     } else {
-      setIsAuthenticated(true);
+      const [headerEncoded, payloadEncoded, signatureEncoded]: string[] =
+        token.split(".");
+
+      const data = `${headerEncoded}.${payloadEncoded}`;
+      const computedSignature = createHmac(
+        "sha256",
+        process.env.NEXT_PUBLIC_APP_SECRET ?? ""
+      )
+        .update(data)
+        .digest("base64")
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
+
+      const isValid = computedSignature === signatureEncoded;
+      if (isValid) {
+        setIsAuthenticated(true);
+      } else {
+        localStorage.removeItem("token");
+        router.push("/");
+      }
     }
   }, [router]);
 
