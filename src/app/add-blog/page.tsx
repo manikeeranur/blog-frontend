e26acf5c8570afb28eb0360920396f0c92d75399
Manikandan Arumugam <manikandan.arumugam@impacteers.com>
2025-07-27 14:02:25 +0530

    bug fix 02

--- src/app/add-blog/page.tsx
+++ src/app/add-blog/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import BlogForm from "@/src/components/blog/blog-form";
 import { useBlog } from "@/src/context/BlogContext";
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 
 const ParentComponent: React.FC = () => {
-
   const { selectedObject } = useBlog();
   return (
     <div className="container">
-      <BlogForm selectedObject={selectedObject} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BlogForm selectedObject={selectedObject} />
+      </Suspense>
     </div>
   );
 };
