14982072bddcb5bf341340a52540406a63314baf
Manikandan Arumugam <manikandan.arumugam@impacteers.com>
2025-07-27 13:55:59 +0530

    bug fix

--- src/app/add-blog/page.tsx
+++ src/app/add-blog/page.tsx
@@ -1,14 +1,10 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import BlogForm from "@/src/components/blog/blog-form";
-import CustomImageCropper from "@/src/components/common/custom-image-cropper";
 import { useBlog } from "@/src/context/BlogContext";
-
-import { Pencil, Upload } from "lucide-react";
 import React, { useState } from "react";
 
 const ParentComponent: React.FC = () => {
-  const [imageUrls, setImageUrls] = useState<any>("");
+
   const { selectedObject } = useBlog();
   return (
     <div className="container">
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,6 @@
 "use client";
+import Blog from "@/src/components/blog/blog";
 import ProtectedRoute from "@/src/components/ProtectedRoute";
-import dynamic from "next/dynamic";
-const Blog = dynamic(() => import("@/src/components/blog/blog"), {
-  ssr: false,
-});
 
 export default function Page() {
   return (
